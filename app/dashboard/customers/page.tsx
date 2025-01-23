eaea1c259794fc81917bb07469a200587d17bf2a
long <yl_whu@163.com>
2025-01-23 15:37:58 +0800

    chapter 10 finish

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
+export default async function Page() {
+  // await new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve(1)
+  //   }, 1000);
+  // })
   return <p>Customers Page</p>;
 }
